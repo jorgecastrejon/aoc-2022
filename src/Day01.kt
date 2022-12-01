b727f39c55d57b35262627905eb23754b6531a90
jorge castrejon <jcastrejon891@gmail.com>
2022-12-01 19:45:46 +0100

    adding solutions to day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.joinToString(",")
+            .split(",,")
+            .maxOf { it.split(",").sumOf(String::toInt) }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int =
+        input.joinToString(",")
+            .split(",,")
+            .map { it.split(",").sumOf(String::toInt) }
+            .sortedDescending()
+            .take(3)
+            .sum()
+
 
     val input = readInput("Day01")
     println(part1(input))
