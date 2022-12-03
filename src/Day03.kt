6e87522b005d1777b8a27d3e25075e81b8936e9c
jorge castrejon <jcastrejon891@gmail.com>
2022-12-03 14:02:45 +0100

    refactoring day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,11 +1,11 @@
 fun main() {
 
     fun part1(input: List<String>): Int =
-        input.sumOf { rucksack -> findMisplacedItem(rucksack)?.priority ?: 0 }
+        input.sumOf { rucksack -> findMisplacedItem(rucksack).priority }
 
 
     fun part2(input: List<String>): Int =
-        input.windowed(3, 3).sumOf { group -> findBadge(group)?.priority ?: 0 }
+        input.chunked(3).sumOf { group -> findBadge(group).priority }
 
 
     val input = readInput("Day03")
@@ -13,31 +13,15 @@ fun main() {
     println(part2(input))
 }
 
-enum class Item { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z }
+val letters = CharRange('a','z')
 
 val Char.priority: Int
-    get() = (Item.values().find { it.name == lowercase() }?.ordinal ?: 0) + 1 + if (isUpperCase()) 26 else 0
+    get() = letters.indexOf(lowercaseChar()) + 1 + if (isUpperCase()) 26 else 0
 
-fun findMisplacedItem(rucksack: String): Char? {
-    val firstSet = hashSetOf<Char>()
-    val secondSet = hashSetOf<Char>()
-    val maxIterations = rucksack.length / 2
+fun findMisplacedItem(rucksack: String): Char =
+    rucksack.chunked(rucksack.length / 2, CharSequence::toSet).reduce(::intersect).first()
 
-    for (index in 0 until maxIterations) {
-        val firstItem = rucksack[index]
-        val secondItem = rucksack[index + maxIterations]
+fun findBadge(group: List<String>): Char =
+    group.map(CharSequence::toSet).reduce(::intersect).first()
 
-        if (!secondSet.contains(firstItem)) firstSet.add(firstItem) else return firstItem
-        if (!firstSet.contains(secondItem)) secondSet.add(secondItem) else return secondItem
-    }
-
-    return null
-}
-
-fun findBadge(group: List<String>): Char? =
-    group.foldIndexed(hashMapOf<Char, MutableSet<Int>>()) { index, hash, rucksack ->
-        rucksack.filter { item -> hash[item]?.contains(index) != true }
-            .forEach { item -> hash.getOrPut(item) { mutableSetOf() }.run { add(index) } }
-
-        hash
-    }.let { hash -> hash.keys.find { hash[it]?.size == 3 } }
+fun intersect(one: Set<Char>, another: Set<Char>): Set<Char> = one.intersect(another)
