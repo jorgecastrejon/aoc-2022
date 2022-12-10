2358951078cf8971a5390615954153166fc20f6d
jorge castrejon <7059764+jorgecastrejon@users.noreply.github.com>
2022-12-10 10:52:35 +0100

    refactoring day 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,53 +1,20 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val instructions = getInstructions(input)
-        var x = 1
-        var cycle = 1
-        val strength = mutableListOf<Int>()
+    fun part1(input: List<String>): Int =
+        getInstructions(input).foldIndexed(1 to mutableListOf<Int>()) { index, (x, strengths), (instruction, value) ->
+            if (((index + 1) - 20) % 40 == 0) strengths.add(x * (index + 1))
+            val x1 = if (instruction == "addx" && value != null) x + value else x
 
-        instructions.forEach { (instruction, value) ->
-
-            if (cycle == 20) {
-                strength.add(x * cycle)
-            }
-
-            if (cycle > 20 &&(cycle -20) % 40 == 0) {
-                strength.add(x * cycle)
-            }
-
-            if (instruction == "addx" && value != null) {
-                x += value
-            }
-
-            cycle++
-        }
-
-        return strength.sum()
-    }
+            x1 to strengths
+        }.second.sum()
 
     fun part2(input: List<String>): Int {
-        val instructions = getInstructions(input)
-        var x = 1
-        var cycle = 1
-        var row = 0
-        val pixels = mutableMapOf<Int, MutableList<String>>()
+        getInstructions(input).foldIndexed(1 to mutableListOf<String>()) { index, (x, pixels), (instruction, value) ->
+            pixels.add(if (((index + 1) % 40) in (x until x + 3)) "#" else ".")
+            val x1 = if (instruction == "addx" && value != null) x + value else x
 
-        instructions.forEach { (instruction, value) ->
-            val pixel = if ((cycle % 40) in (x until x + 3) ) "#" else "."
-            pixels.getOrPut(row) { mutableListOf() }.add(pixel)
-
-            if (cycle % 40 == 0) {
-                row++
-            }
-
-            if (instruction == "addx" && value != null) {
-                x += value
-            }
-
-            cycle++
-        }
-        pixels.values.forEach { println(it.joinToString("")) }
+            x1 to pixels
+        }.second.chunked(40).forEach { line -> println(line.joinToString("")) }
 
         return 0
     }
@@ -57,19 +24,13 @@ fun main() {
     println(part2(input))
 }
 
-private fun getInstructions(input: List<String>): MutableList<Pair<String, Int?>> {
-    val instructions = mutableListOf<Pair<String, Int?>>()
-
-    for (line in input) {
-        val split = line.split(" ")
-
-        if (split.first() == "addx") {
-            instructions.add(split.first() to null)
-            instructions.add(split.first() to split.last().toIntOrNull())
-        } else {
-            instructions.add(split.first() to null)
+private fun getInstructions(input: List<String>): List<Pair<String, Int?>> =
+    input.map { line ->
+        line.split(" ").let {
+            if (it.first() == "addx") {
+                listOf(it.first() to null, it.first() to it.last().toIntOrNull())
+            } else {
+                listOf(it.first() to null)
+            }
         }
-    }
-
-    return instructions
-}
+    }.flatten()
